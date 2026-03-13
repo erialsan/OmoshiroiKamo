4625b651ce6801df5821a912f5f3d841b96f786d
Ruisu <akitomoya2908@gmail.com>
2026-03-13 23:18:15 +0700

    update depen

--- src/main/java/ruiseki/omoshiroikamo/core/lib/LibMisc.java
+++ src/main/java/ruiseki/omoshiroikamo/core/lib/LibMisc.java
@@ -9,7 +9,7 @@ public final class LibMisc {
     public static final String MOD_NAME = Tags.MOD_NAME;
 
     public static final String VERSION = Tags.VERSION;
-    public static final String DEPENDENCIES = "required-after:gtnhlib@[0.9.5,);"
+    public static final String DEPENDENCIES = "required-after:gtnhlib@[0.9.40,);"
         + "required-after:structurelib@[1.4.28,);"
         + "required-after:modularui2@[2.3.41-1.7.10,);"
         + "after:Baubles|Expanded;"
@@ -30,5 +30,10 @@ public final class LibMisc {
     public static final String PROXY_COMMON = Tags.MOD_GROUP + ".CommonProxy";
     public static final String PROXY_CLIENT = Tags.MOD_GROUP + ".ClientProxy";
     public static final String GUI_FACTORY = Tags.MOD_GROUP + ".config.OKGuiConfigFactory";
+
+    /**
+     * use {@link ruiseki.omoshiroikamo.OmoshiroiKamo}.okLog
+     */
+    @Deprecated
     public static final LangUtils LANG = new LangUtils();
 }
