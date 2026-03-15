cb53b6036b0ea1fc61c1b8d3b5aff32d073382e8
Ruisu <akitomoya2908@gmail.com>
2026-03-15 20:03:54 +0700

    fix

--- src/main/java/ruiseki/omoshiroikamo/module/backpack/integration/nei/BackpackOverlay.java
+++ src/main/java/ruiseki/omoshiroikamo/module/backpack/integration/nei/BackpackOverlay.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
